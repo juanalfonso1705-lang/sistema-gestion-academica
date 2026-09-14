08bb925bde848c2ddc885b555a0ed59cc3714f08
Juan Alfonso <juanalfonso.1705@gmail.com>
2026-09-14 15:29:49 -0500

    Ejercicio 6: Manejo de excepciones e interpretacion teorica

--- validaciones.py
+++ validaciones.py
@@ -13,3 +13,14 @@ from validaciones import validar_nota
 
 print(validar_nota(4.5))
 print(validar_nota(7))
+
+try:
+    nota = float(input("Digite la nota: "))
+
+    if nota < 0 or nota > 5:
+        print("La nota debe estar entre 0 y 5.")
+    else:
+        print("Nota válida.")
+
+except ValueError:
+    print("Error: debe ingresar un número.")
